aa97242b649f5a5fd1340d8e32656fb02b598d87
abzdevs18 <38336247+abzdevs18@users.noreply.github.com>
2018-06-24 21:33:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/debian/pointopoint/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.debian.pointopoint;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.jgabrielfreitas.core.BlurImageView;
+
+import java.nio.channels.Channel;
+
+public class MainActivity extends AppCompatActivity {
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+    private TextView msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        final BlurImageView blurImageView = (BlurImageView) findViewById(R.id.default_bck_ground);
+        blurImageView.setBlur(2);
+//        SeekBar seekBar = findViewById(R.id.seekBar);
+//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                blurImageView.setBlur(progress / 4); // the max value of a setBlur is 25 / the max value of a  seekbar
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
+        initialize();
+    }
+
+    private void initialize() {
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, (WifiP2pManager.Channel) mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+}
---
+++ app/src/main/java/com/example/debian/pointopoint/SwipeButton.java
@@ -0,0 +1,267 @@
+package com.example.debian.pointopoint;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import static android.support.v4.content.ContextCompat.startActivity;
+
+public class SwipeButton extends RelativeLayout {
+    private ImageView slidingButton;
+    private float initialX;
+    private boolean active;
+    private int initialButtonWidth;
+    private TextView centerText;
+    public static Context mContext;
+    private Drawable disabledDrawable;
+    private Drawable enabledDrawable;
+
+    private SwipeButton swipeButton, sendBtn;
+
+    public SwipeButton(Context context) {
+        super(context);
+
+        init(context, null, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, defStyleAttr);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(Context context, AttributeSet attrs, int i, int i1) {
+        RelativeLayout background = new RelativeLayout(context);
+        LayoutParams layoutParamsView = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParamsView.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+//        sendBtn = findViewById(R.id.swipe_btn);
+        swipeButton = findViewById(R.id.swipe_btn_receive);
+
+        background.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_rounded));
+        addView(background, layoutParamsView);
+
+        //center text of the button
+        final TextView centerText = new TextView(context);
+        this.centerText = centerText;
+        centerText.setGravity(Gravity.CENTER);
+
+        LayoutParams layoutParams = new LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        centerText.setText(R.string.swipe_to_send); //add any text you need
+        centerText.setTextColor(Color.WHITE);
+        centerText.setPadding(20, 20, 20, 20);// this makes the background of the button big
+        background.addView(centerText, layoutParams);
+
+        final ImageView swipeButton = new ImageView(context);
+        this.slidingButton = swipeButton;
+
+        disabledDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_keyboard_arrow_right_black_24dp);
+        enabledDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_keyboard_arrow_left_black_24dp);
+
+        slidingButton.setImageDrawable(disabledDrawable);
+        slidingButton.setPadding(6, 6, 6, 6);
+
+        LayoutParams layoutParamsButton = new LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParamsButton.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        layoutParamsButton.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+        swipeButton.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_button));
+        swipeButton.setImageDrawable(disabledDrawable);
+        addView(swipeButton, layoutParamsButton);
+
+        setOnTouchListener(getButtonTouchListener());
+    }
+
+    private OnTouchListener getButtonTouchListener() {
+        return new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("action down","Activate the down");
+
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Movement logic here
+                        if (initialX == 0) {
+                            initialX = slidingButton.getX();
+                        }
+                        if (event.getX() > initialX + slidingButton.getWidth() / 2 &&
+                                event.getX() + slidingButton.getWidth() / 2 < getWidth()) {
+                            slidingButton.setX(event.getX() - slidingButton.getWidth() / 2);
+                            centerText.setAlpha(1 - 1.3f * (slidingButton.getX() + slidingButton.getWidth()) / getWidth());
+                        }
+
+                        if  (event.getX() + slidingButton.getWidth() / 2 > getWidth() &&
+                                slidingButton.getX() + slidingButton.getWidth() / 2 < getWidth()) {
+                            slidingButton.setX(getWidth() - slidingButton.getWidth());
+                        }
+
+                        if  (event.getX() < slidingButton.getWidth() / 2 &&
+                                slidingButton.getX() > 0) {
+                            slidingButton.setX(5);
+                        }
+                        Log.d("tag","Activate the Movement");
+
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        //Release logic here
+                        if (active) {
+                            collapseButton();
+                        } else {
+                            initialButtonWidth = slidingButton.getWidth();
+
+                            if (slidingButton.getX() + slidingButton.getWidth() > getWidth() * 0.85) {
+                                expandButton();
+//                                mContext get the context of this class and start it when the slide is release
+                                mContext = getContext();
+                                Intent myIntent = new Intent(mContext, Receive.class);
+                                mContext.startActivity(myIntent);
+
+                                Log.d("tag","Activate the action");
+                            } else {
+                                moveButtonBack();
+                            }
+                        }
+                        return true;
+                }
+                return false;
+            }
+        };
+    }
+
+
+    private void expandButton() {
+//        First, the slidingButton is going to animate do the left edge of our component. That's the positionAnimator.
+        final ValueAnimator positionAnimator =
+                ValueAnimator.ofFloat(slidingButton.getX(), 0);
+        positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (Float) positionAnimator.getAnimatedValue();
+                slidingButton.setX(x);
+            }
+        });
+
+//        Second, the button must expand and occupy the entire space of the component. That the widthAnimator.
+        final ValueAnimator widthAnimator = ValueAnimator.ofInt(
+                slidingButton.getWidth(),
+                getWidth());
+
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ViewGroup.LayoutParams params = slidingButton.getLayoutParams();
+                params.width = (Integer) widthAnimator.getAnimatedValue();
+                slidingButton.setLayoutParams(params);
+            }
+        });
+
+//        Third, we create an AnimatorSet so we can play the animations together.
+//        At the end of the animation, we set the state of the button as active and change the image of the button.
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                active = true;
+                slidingButton.setImageDrawable(enabledDrawable);
+            }
+        });
+
+        animatorSet.playTogether(positionAnimator, widthAnimator);
+        animatorSet.start();
+    }
+//    This animation is similar to the expand button, but this time we increase the alpha of the text so it can become visible again.
+    private void collapseButton() {
+        final ValueAnimator widthAnimator = ValueAnimator.ofInt(
+                slidingButton.getWidth(),
+                initialButtonWidth);
+
+        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ViewGroup.LayoutParams params =  slidingButton.getLayoutParams();
+                params.width = (Integer) widthAnimator.getAnimatedValue();
+                slidingButton.setLayoutParams(params);
+            }
+        });
+
+        widthAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                active = false;
+                slidingButton.setImageDrawable(disabledDrawable);
+            }
+        });
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(
+                centerText, "alpha", 1);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        animatorSet.playTogether(objectAnimator, widthAnimator);
+        animatorSet.start();
+    }
+//    This animation move the button back to the left edge and recovers the alpha of the component text.
+    private void moveButtonBack() {
+        final ValueAnimator positionAnimator =
+                ValueAnimator.ofFloat(slidingButton.getX(), 0);
+        positionAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        positionAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float x = (Float) positionAnimator.getAnimatedValue();
+                slidingButton.setX(x);
+            }
+        });
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(
+                centerText, "alpha", 1);
+
+        positionAnimator.setDuration(200);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(objectAnimator, positionAnimator);
+        animatorSet.start();
+    }
+}
+
